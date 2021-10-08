8fd5d2f653e06e67a6440c1174bd9bbe1dd91d73
Todd Nordquist <toddnordquist144@gmail.com>
2021-10-08 12:07:43 -0600

    Added listener to spinner: displays image.

--- app/src/main/java/edu/cnm/deepdive/animals14/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals14/MainActivity.java
@@ -2,9 +2,14 @@ package edu.cnm.deepdive.animals14;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
+import com.squareup.picasso.Picasso;
 import edu.cnm.deepdive.animals14.model.Animal;
 import edu.cnm.deepdive.animals14.service.WebServiceProxy;
 import java.io.IOException;
@@ -15,12 +20,29 @@ public class MainActivity extends AppCompatActivity {
 
   private Spinner animalSelector;
   private ArrayAdapter<Animal> adapter;
+  private ImageView image;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     animalSelector = findViewById(R.id.animal_selector);
+    image = findViewById(R.id.image);
+    animalSelector.setOnItemSelectedListener(new OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Animal animal = (Animal) parent.getItemAtPosition(position);
+        if (animal.getImageUrl() != null) {
+          Picasso.get().load(String.format("%s/content", animal.getImageUrl()))
+              .into((ImageView) findViewById(R.id.image));
+        }
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> parent) {
+
+      }
+    });
     new Retriever().start();
   }
 
@@ -36,14 +58,16 @@ public class MainActivity extends AppCompatActivity {
         if (response.isSuccessful()) {
           Log.d(getClass().getName(), response.body().toString());
           List<Animal> animals = response.body();
-          runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-              adapter = new ArrayAdapter<>(MainActivity.this, R.layout.item_animal_spinner,
-                  animals);
-              adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-              animalSelector.setAdapter(adapter);
+          String url = animals.get(0).getImageUrl();
+          runOnUiThread(() -> {
+            adapter = new ArrayAdapter<>(MainActivity.this, R.layout.item_animal_spinner,
+                animals);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+            if (url != null) {
+              Picasso.get().load(String.format("%s/content", url))
+                  .into((ImageView) findViewById(R.id.image));
             }
+            animalSelector.setAdapter(adapter);
           });
         } else {
           Log.e(getClass().getName(), response.message());
